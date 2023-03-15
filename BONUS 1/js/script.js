b19c0333674cec2d0c8e670d810352cfd10ea844
nicolo fossati <nicofossa01@gmail.com>
2023-03-15 14:46:53 +0100

    BONUS 1 COMPLETO

--- BONUS 1/js/script.js
+++ BONUS 1/js/script.js
@@ -1,9 +1,6 @@
 /*
-Descrizione:
+Bonus:
     1- al click su una thumb, visualizzare in grande l’immagine corrispondente
-    
-    2- applicare l’autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente
-    3- quando il mouse va in hover sullo slider, bloccare l’autoplay e farlo riprendere quando esce
 */
 
 const { createApp } = Vue
@@ -53,6 +50,10 @@ const { createApp } = Vue
             } else {
                 this.currentImg++;
             }
+        },
+
+        changeImg(index){
+            this.currentImg = index;
         }
     }
   }).mount('#app')
